bde3bbdfc3e8b47c65637381e21d912907f1fcd2
Slim Hmidi <slim.hmidi1@gmail.com>
2019-10-17 15:14:47 +0200

    Updates

---
+++ precache-manifest.a2609a933b9f326a937e21facadd05a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4f454e5c7390ac8ab654f84095f73e",
+    "url": "/slim-hmidi/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "75c881b4d4c873447899",
+    "url": "/slim-hmidi/monsters-rolodex/static/css/main.10c3f28b.chunk.css"
+  },
+  {
+    "revision": "8f930e99e4bacbe3126c",
+    "url": "/slim-hmidi/monsters-rolodex/static/js/2.7397039c.chunk.js"
+  },
+  {
+    "revision": "75c881b4d4c873447899",
+    "url": "/slim-hmidi/monsters-rolodex/static/js/main.475b2909.chunk.js"
+  },
+  {
+    "revision": "6ab48a7cb605bbc6efbb",
+    "url": "/slim-hmidi/monsters-rolodex/static/js/runtime-main.ae85782e.js"
+  }
+]);
